264250bc820141a139b85e0ea2f0723e161bfdcc
Tanveer Y <tanveery@users.noreply.github.com>
2020-01-10 16:42:51 +0530

    Added Stripe integration project.

---
+++ src/Financials/FinancialsPaymentStripe/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ejyle.DevAccelerate.Financials.Payment.Stripe")]
+[assembly: AssemblyDescription("Provides Stripe integration for DevAccelerate Financials library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ejyle Technologies (P) Ltd")]
+[assembly: AssemblyProduct("Ejyle.DevAccelerate.Financials.Payment.Stripe")]
+[assembly: AssemblyCopyright("Copyright © Ejyle Technologies (P) Ltd. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f574fef-6053-4324-bad9-d35db8bd50da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0")]
