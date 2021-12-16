59012d0da3e084a6df34ed64e60185a07f53cea3
Tanveer Yousuf <tanveery@outlook.com>
2021-12-16 22:58:56 +0530

    Renamed DaDefaultEntityKeyConverter as DaIntegerKeyConverter

---
+++ src/Core/Ejyle.DevAccelerate.Core/DaIntegerKeyConverter.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+namespace Ejyle.DevAccelerate.Core
+{
+    /// <summary>
+    /// Represents the interface to convert int to int? and vice-versa.
+    /// </summary>
+    public class DaIntegerKeyConverter : IDaEntityKeyConverter<int, int?>
+    {
+        /// <summary>
+        /// Converts int to int?.
+        /// </summary>
+        /// <param name="value">The int value that needs to be converted to int?.</param>
+        /// <returns>Returns the value as int?.</returns>
+        public int? ToNullableKey(int value)
+        {
+            int? intValue = value;
+            return intValue;
+        }
+
+        /// <summary>
+        /// Converts int? to int.
+        /// </summary>
+        /// <param name="value">The int? value that needs to be converted to int.</param>
+        /// <returns>Returns the value as an int.</returns>
+        public int ToKey(int? value)
+        {
+            int intValue = (int)value;
+            return intValue;
+        }
+    }
+}
