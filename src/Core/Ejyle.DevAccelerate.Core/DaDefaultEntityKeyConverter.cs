a64c20fdd2640eb4bac060d4cf60dca8f22249cb
Tanveer Yousuf <tanveery@outlook.com>
2021-12-16 21:29:51 +0530

    Added IDaEntityKeyConverter interface and its default implementation

---
+++ src/Core/Ejyle.DevAccelerate.Core/DaDefaultEntityKeyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejyle.DevAccelerate.Core
+{
+    public class DaDefaultEntityKeyConverter : IDaEntityKeyConverter<int, int?>
+    {
+        public int? ToNullableKey(int value)
+        {
+            int? intValue = value;
+            return intValue;
+        }
+
+        public int ToKey(int? value)
+        {
+            int intValue = (int)value;
+            return intValue;
+        }
+    }
+}
