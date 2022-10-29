0f404f61876e6eef5f3950ae69ce7ebc17842714
Tanveer Yousuf <tanveery@users.noreply.github.com>
2022-10-29 11:32:18 +0530

    Add interfaces and classes for tracking objects.

---
+++ src/EnterpriseSecurity/Ejyle.DevAccelerate.EnterpriseSecurity/Objects/DaObjectInstance.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using Ejyle.DevAccelerate.Core;
+using Ejyle.DevAccelerate.EnterpriseSecurity.Apps;
+using Ejyle.DevAccelerate.EnterpriseSecurity.SubscriptionPlans;
+using Ejyle.DevAccelerate.EnterpriseSecurity.Subscriptions;
+using System;
+using System.Collections.Generic;
+
+namespace Ejyle.DevAccelerate.EnterpriseSecurity.Objects
+{
+    public class DaObjectInstance : DaObjectInstance<int, DaObjectType, DaObjectHistoryItem>
+    {
+        public DaObjectInstance() : base()
+        { }
+    }
+
+    public class DaObjectInstance<TKey, TObjectType, TObjectHistoryItem>
+        : DaAuditedEntityBase<TKey>, IDaObjectInstance<TKey>
+        where TKey : IEquatable<TKey>
+        where TObjectType : IDaObjectType<TKey>
+        where TObjectHistoryItem : IDaObjectHistoryItem<TKey>
+    {
+        public DaObjectInstance()
+            : base()
+        {
+            ObjectHistoryItems = new HashSet<TObjectHistoryItem>();
+        }
+
+        public TKey ObjectTypeId { get; set; }
+
+        public TKey SourceObjectId { get; set; }
+
+        public virtual TObjectType ObjectType { get; set; }
+
+        public virtual ICollection<TObjectHistoryItem> ObjectHistoryItems { get; set; }
+    }
+}
---
+++ src/EnterpriseSecurity/Ejyle.DevAccelerate.EnterpriseSecurity/Objects/DaObjectType.cs
@@ -0,0 +1,45 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using Ejyle.DevAccelerate.Core;
+using Ejyle.DevAccelerate.EnterpriseSecurity.Apps;
+using Ejyle.DevAccelerate.EnterpriseSecurity.SubscriptionPlans;
+using Ejyle.DevAccelerate.EnterpriseSecurity.Subscriptions;
+using System;
+using System.Collections.Generic;
+
+namespace Ejyle.DevAccelerate.EnterpriseSecurity.Objects
+{
+    public class DaObjectType : DaObjectType<int, DaApp, DaObjectInstance>
+    {
+        public DaObjectType() : base()
+        { }
+    }
+
+    public class DaObjectType<TKey, TApp, TObjectInstance>
+        : DaEntityBase<TKey>, IDaObjectType<TKey>
+        where TKey : IEquatable<TKey>
+        where TApp : IDaApp<TKey>
+        where TObjectInstance : IDaObjectInstance<TKey>
+    {
+        public DaObjectType()
+            : base()
+        {
+            ObjectInstances = new HashSet<TObjectInstance>();
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public TKey AppId { get; set; }
+
+        public virtual TApp App { get; set; }
+
+        public virtual ICollection<TObjectInstance> ObjectInstances { get; set; }
+    }
+}
