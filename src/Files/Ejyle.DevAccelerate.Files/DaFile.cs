3b613b7d9e09bc76180d43b2b84727db45f989b8
Tanveer Yousuf <tanveery@users.noreply.github.com>
2022-10-29 18:53:06 +0530

    Added files module

---
+++ src/Files/Ejyle.DevAccelerate.Files/DaFile.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using Ejyle.DevAccelerate.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejyle.DevAccelerate.Files
+{
+    public class DaFile<TKey, TNullableKey, TFileCollection> : DaAuditedEntityBase<TKey>, IDaFile<TKey, TNullableKey>
+        where TKey : IEquatable<TKey>
+        where TFileCollection : IDaFileCollection<TKey, TNullableKey>
+    {
+        public string FileName { get; set; }
+        public string GuidFileName { get; set; }
+        public string MimeType { get; set; }
+        public long FileSize { get; set; }
+        public TNullableKey FileCollectionId { get; set; }
+        public TKey OwnerUserId { get; set; }
+        public TNullableKey TenantId { get; set; }
+        public virtual TFileCollection FileCollection { get; set; }
+    }
+}
---
+++ src/Files/Ejyle.DevAccelerate.Files/DaFileCollection.cs
@@ -0,0 +1,38 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using Ejyle.DevAccelerate.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejyle.DevAccelerate.Files
+{
+    public class DaFileCollection<TKey, TNullableKey, TFileCollection, TFile> : DaAuditedEntityBase<TKey>, IDaFileCollection<TKey, TNullableKey>
+        where TKey : IEquatable<TKey>
+        where TFileCollection : IDaFileCollection<TKey, TNullableKey>
+        where TFile : IDaFile<TKey, TNullableKey>
+    {
+        public DaFileCollection() : base()
+        {
+            Children = new HashSet<TFileCollection>();
+            Files = new HashSet<TFile>();
+        }
+
+        public string Name { get; set; }
+        public TNullableKey ObjectInstanceId { get; set; }
+        public bool IsUserDefined { get; set; }
+        public TKey OwnerUserId { get; set; }
+        public TNullableKey ParentId { get; set; }
+        public TNullableKey TenantId { get; set; }
+        public virtual TFileCollection Parent { get; set; }
+        public virtual ICollection<TFileCollection> Children { get; set; }
+        public virtual ICollection<TFile> Files { get; set; }
+    }
+}
