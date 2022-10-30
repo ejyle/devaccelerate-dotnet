650e92561ec8dcdf1b58e8e5d5a6caaffd5c735e
Tanveer Yousuf <tanveery@users.noreply.github.com>
2022-10-30 10:42:11 +0530

    Added createfilestorages command

--- src/Tools/Ejyle.DevAccelerate.Tools/Program.cs
+++ src/Tools/Ejyle.DevAccelerate.Tools/Program.cs
@@ -9,6 +9,7 @@ using System;
 using CommandLine;
 using Ejyle.DevAccelerate.Tools.Commands;
 using Ejyle.DevAccelerate.Tools.Commands.EnterpriseSecurity;
+using Ejyle.DevAccelerate.Tools.Commands.Files;
 using Ejyle.DevAccelerate.Tools.Commands.Identity;
 using Ejyle.DevAccelerate.Tools.Commands.Lists;
 
@@ -26,6 +27,7 @@ namespace Ejyle.DevAccelerate.Tools
                     DaCreateAppCommand,
                     DaCreateAppFeatureCommand,
                     DaCreateDefaultListsCommand,
+                    DaCreateDefaultFileStoragesCommand,
                     DaCreateUserCommand,
                     DaCreateRolesCommand,
                     DaAddUserToRolesCommand>(args)
