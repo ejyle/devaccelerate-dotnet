e984ad3c236a499f213caf8ac087166328fb0a2d
Tanveer Y <tanveery@users.noreply.github.com>
2022-12-13 11:20:13 +0530

    Restructured the forms folder structure.

---
+++ src/Forms/Ejyle.DevAccelerate.Forms/Forms/DaForm.cs
@@ -0,0 +1,33 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using Ejyle.DevAccelerate.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejyle.DevAccelerate.Forms.Forms
+{
+    public class DaForm<TKey, TNullableKey, TFormSection> : DaAuditedEntityBase<TKey>, IDaForm<TKey, TNullableKey>
+        where TKey : IEquatable<TKey>
+        where TFormSection : IDaFormSection<TKey>
+    {
+        public DaForm()
+        {
+            Sections = new HashSet<TFormSection>();
+        }
+
+        public TNullableKey TenantId { get; set; }
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public virtual ICollection<TFormSection> Sections { get; set; }
+    }
+}
---
+++ src/Forms/Ejyle.DevAccelerate.Forms/Instances/IDaFormInstance.cs
@@ -0,0 +1,24 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using Ejyle.DevAccelerate.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejyle.DevAccelerate.Forms.Instances
+{
+    public interface IDaFormInstance<TKey, TNullableKey> : IDaAuditedEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        TKey FormId { get; set; }
+        TKey OwnerUserId { get; set; }
+        TNullableKey TenantId { get; set; }
+    }
+}
