aea23ae88b17261b3ab8a7e0ea58f4950b0fd866
Tanveer Y <tanveery@users.noreply.github.com>
2022-12-29 23:27:09 +0530

    Added DaDuplicateRecordException class.

---
+++ src/Core/Ejyle.DevAccelerate.Core/DaDuplicateRecordException.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Ejyle.DevAccelerate.Core
+{
+    /// <summary>
+    /// Represents an exception when a to-be-created record already exists.
+    /// </summary>
+    public class DaDuplicateRecordException : ApplicationException
+    {
+        /// <summary>
+        /// Creates an instance of the <see cref="DaDuplicateRecordException"/> class.
+        /// </summary>
+        public DaDuplicateRecordException() { }
+
+        /// <summary>
+        /// Creates an instance of the <see cref="DaDuplicateRecordException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public DaDuplicateRecordException(string message) : base(message) { }
+    }
+}
