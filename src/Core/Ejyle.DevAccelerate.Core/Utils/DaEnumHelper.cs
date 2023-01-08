9e24416a1aeab82225440c8036615d34cc7d450e
Tanveer Y <tanveery@users.noreply.github.com>
2023-01-08 11:46:26 +0530

    Added DaEnumHelper

---
+++ src/Core/Ejyle.DevAccelerate.Core/Utils/DaEnumHelper.cs
@@ -0,0 +1,75 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Author: Tanveer Yousuf (@tanveery)
+// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejyle.DevAccelerate.Core.Utils
+{
+    public class DaEnumHelper<T>
+        where T : struct, Enum
+    {
+        public static IList<T> GetValues(Enum value)
+        {
+            var enumValues = new List<T>();
+
+            foreach (var field in value.GetType().GetFields(BindingFlags.Static | BindingFlags.Public))
+            {
+                enumValues.Add((T)Enum.Parse(value.GetType(), field.Name, false));
+            }
+
+            return enumValues;
+        }
+
+        public static T Parse(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        public static IList<string> GetNames(Enum value)
+        {
+            return value.GetType().GetFields(BindingFlags.Static | BindingFlags.Public).Select(fi => fi.Name).ToList();
+        }
+
+        public static IList<string> GetDisplayValues(Enum value)
+        {
+            return GetNames(value).Select(obj => GetDisplayValue(Parse(obj))).ToList();
+        }
+
+        private static string LookupResource(Type resourceManagerProvider, string resourceKey)
+        {
+            var resourceKeyProperty = resourceManagerProvider.GetProperty(resourceKey,
+                BindingFlags.Static | BindingFlags.Public, null, typeof(string),
+                new Type[0], null);
+            if (resourceKeyProperty != null)
+            {
+                return (string)resourceKeyProperty.GetMethod.Invoke(null, null);
+            }
+
+            return resourceKey;
+        }
+
+        public static string GetDisplayValue(T value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var descriptionAttributes = fieldInfo.GetCustomAttributes(
+                typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+            if (descriptionAttributes[0].ResourceType != null)
+                return LookupResource(descriptionAttributes[0].ResourceType, descriptionAttributes[0].Name);
+
+            if (descriptionAttributes == null) return string.Empty;
+            return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
+        }
+    }
+}
